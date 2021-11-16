ac67085bff06514fffc9ed888f575b5eaa4fc67c
Francisco Herrera <fran.herrera124@gmail.com>
2021-11-16 17:15:35 -0300

    added missing commas

--- src/app/modules/study/study.module.ts
+++ src/app/modules/study/study.module.ts
@@ -1,25 +1,28 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { InlineSVGModule } from 'ng-inline-svg';
-import { CRUDTableModule } from '../../_metronic/shared/crud-table';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { NgbDatepickerModule, NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
-import { EditStudyModalComponent } from './study-list/components/edit-study-modal/edit-study-modal.component';
-import { StudyRoutingModule } from './study-routing.module';
-import { StudyComponent } from './study.component';
-import { StudyListComponent } from './study-list/study-list.component';
-import { SharedModule } from '../shared/shared.module';
-import { ConsentUploadModalComponent } from './study-list/components/consent-upload-modal/consent-upload-modal.component';
-import {MatIconModule} from '@angular/material/icon';
-import { PaymentUploadModalComponent } from './study-list/components/payment-upload-modal/payment-upload-modal.component';
-import { ShiftReservationModalComponent } from './study-list/components/shift-reservation-modal/shift-reservation-modal.component';
-import { RegisterSampleModalComponent} from './study-list/components/register-sample-modal/register-sample-modal.component';
-import { RegisterSamplePickupModalComponent } from './study-list/components/register-sample-pickup-modal/register-sample-pickup-modal';
-import { RegisterReportModalComponent } from './study-list/components/register-report-modal/register-report-modal.component';
-import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
-import { ConfirmSendReportModalComponent } from './study-list/components/confirm-send-report-modal/confirm-send-rerport-modal';
-import { DetailStudyModalComponent } from './study-list/components/detail-study-modal/detail-study-modal.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { InlineSVGModule } from "ng-inline-svg";
+import { CRUDTableModule } from "../../_metronic/shared/crud-table";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import {
+  NgbDatepickerModule,
+  NgbModalModule,
+} from "@ng-bootstrap/ng-bootstrap";
+import { EditStudyModalComponent } from "./study-list/components/edit-study-modal/edit-study-modal.component";
+import { StudyRoutingModule } from "./study-routing.module";
+import { StudyComponent } from "./study.component";
+import { StudyListComponent } from "./study-list/study-list.component";
+import { SharedModule } from "../shared/shared.module";
+import { ConsentUploadModalComponent } from "./study-list/components/consent-upload-modal/consent-upload-modal.component";
+import { MatIconModule } from "@angular/material/icon";
+import { PaymentUploadModalComponent } from "./study-list/components/payment-upload-modal/payment-upload-modal.component";
+import { ShiftReservationModalComponent } from "./study-list/components/shift-reservation-modal/shift-reservation-modal.component";
+import { RegisterSampleModalComponent } from "./study-list/components/register-sample-modal/register-sample-modal.component";
+import { RegisterSamplePickupModalComponent } from "./study-list/components/register-sample-pickup-modal/register-sample-pickup-modal";
+import { RegisterReportModalComponent } from "./study-list/components/register-report-modal/register-report-modal.component";
+import { FroalaEditorModule, FroalaViewModule } from "angular-froala-wysiwyg";
+import { ConfirmSendReportModalComponent } from "./study-list/components/confirm-send-report-modal/confirm-send-rerport-modal";
+import { DetailStudyModalComponent } from "./study-list/components/detail-study-modal/detail-study-modal.component";
 
 @NgModule({
   declarations: [
@@ -32,9 +35,8 @@ import { DetailStudyModalComponent } from './study-list/components/detail-study-
     RegisterSampleModalComponent,
     RegisterSamplePickupModalComponent,
     RegisterReportModalComponent,
-    ConfirmSendReportModalComponent
-    DetailStudyModalComponent
-
+    ConfirmSendReportModalComponent,
+    DetailStudyModalComponent,
   ],
   imports: [
     CommonModule,
@@ -48,8 +50,8 @@ import { DetailStudyModalComponent } from './study-list/components/detail-study-
     SharedModule,
     MatDatepickerModule,
     MatIconModule,
-    FroalaEditorModule.forRoot(), FroalaViewModule.forRoot()
-    
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
   ],
   entryComponents: [
     EditStudyModalComponent,
@@ -58,12 +60,11 @@ import { DetailStudyModalComponent } from './study-list/components/detail-study-
     ShiftReservationModalComponent,
     RegisterSampleModalComponent,
     RegisterSamplePickupModalComponent,
-    ConfirmSendReportModalComponent
-    DetailStudyModalComponent
+    ConfirmSendReportModalComponent,
+    DetailStudyModalComponent,
   ],
-  exports: [ FroalaEditorModule, FroalaViewModule],
+  exports: [FroalaEditorModule, FroalaViewModule],
 
-
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class StudyModule {}
