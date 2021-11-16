6ed42c416e5f69f6cf35e9daead1b254de6e16c9
Francisco <fran.herrera124@gmail.com>
2021-11-16 10:57:42 -0300

    Fix api url for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   appVersion: 'v717demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'api'
+  apiUrl: 'http://sweethomie.ddns.net/api/v1'
   // apiUrl: 'mysite.com/api'
 };
