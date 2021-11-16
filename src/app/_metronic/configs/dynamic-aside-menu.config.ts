ef8c62f5b1ecc1490317e452cd061e308d67a9ed
Francisco Herrera <fran.herrera124@gmail.com>
2021-11-15 23:59:42 -0300

    initial commit

---
+++ src/app/_metronic/configs/dynamic-aside-menu.config.ts
@@ -0,0 +1,88 @@
+export const DynamicAsideMenuConfig = {
+  items: [
+    {
+      title: 'Home',
+      root: true,
+      icon: 'flaticon2-architecture-and-city',
+      svg: './assets/media/svg/icons/Design/Layers.svg',
+      page: '/dashboard',
+      translate: 'MENU.DASHBOARD',
+      bullet: 'dot',
+    },
+    { section: 'Applications' },
+    {
+      title: 'User Management',
+      root: true,
+      bullet: 'dot',
+      icon: 'flaticon2-user-outline-symbol',
+      svg: './assets/media/svg/icons/General/User.svg',
+      page: '/user-management',
+      submenu: [
+        {
+          title: 'Users',
+          page: '/user-management/users'
+        },
+        {
+          title: 'Roles',
+          page: '/user-management/roles'
+        }
+      ]
+    },
+    {
+      title: 'Pacientes',
+      root: true,
+      bullet: 'dot',
+      icon: 'flaticon2-user-outline-symbol',
+      svg: './assets/media/svg/icons/General/User.svg',
+      page: '/patients',
+      submenu: [
+        {
+          title: 'Lista de Pacientes',
+          page: '/patients/patients'
+        },
+      ]
+    },
+    {
+      title: 'Estudios',
+      root: true,
+      bullet: 'dot',
+      icon: 'flaticon2-user-outline-symbol',
+      svg: './assets/media/svg/icons/General/User.svg',
+      page: '/studies',
+      submenu: [
+        {
+          title: 'Lista de Estudios',
+          page: '/studies/studies'
+        },
+      ]
+    },
+    {
+      title: 'Lotes',
+      root: true,
+      bullet: 'dot',
+      icon: 'flaticon2-user-outline-symbol',
+      svg: './assets/media/svg/icons/General/User.svg',
+      page: '/studies',
+      submenu: [
+        {
+          title: 'Lista de Lotes',
+          page: '/sample-batches/sample-batches'
+        },
+      ]
+    },
+    {
+      title: 'Tipo de estudios',
+      root: true,
+      bullet: 'dot',
+      icon: 'flaticon2-user-outline-symbol',
+      svg: './assets/media/svg/icons/General/User.svg',
+      page: '/type-studies',
+      submenu: [
+        {
+          title: 'Lista de tipo de estudios',
+          page: '/type-studies/type-studies'
+        },
+      ]
+    }
+  ]
+};
