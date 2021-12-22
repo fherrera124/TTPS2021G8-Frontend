177eba70ba3d38e30d8a86bcae3b3e42c975f5a5
german flores <german.flores@tecso.coop>
2021-12-22 19:06:36 -0300

    cambios en fechas y configurador

--- src/app/modules/shared/service/configurator.service.ts
+++ src/app/modules/shared/service/configurator.service.ts
@@ -8,10 +8,16 @@ import { Observable } from 'rxjs';
 })
 export class ConfiguratorService {
   API_URL = `${environment.apiUrl}/configurators/configuration/get`;
+  API_URL_CONFIG_UPDATE = `${environment.apiUrl}/configurators/update-configuration`;
   constructor(public http: HttpClient) {
   }
 
   getConfigurator(): Observable<any> {
     return this.http.get(this.API_URL);
+  }
+
+  updateConfigurations(mode: boolean): Observable<any> {
+    let p = {"obligated_mode":mode}
+    return this.http.put(this.API_URL_CONFIG_UPDATE,p);
   }
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService, UserModel } from 'src/app/modules/auth';
-import { AuthModel } from 'src/app/modules/auth/_models/auth.model';
-
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+import { Component, OnInit } from "@angular/core";
+import { delay } from "rxjs/operators";
+import { AuthService, UserModel } from "src/app/modules/auth";
+import { AuthModel } from "src/app/modules/auth/_models/auth.model";
+import { ConfiguratorService } from "src/app/modules/shared/service/configurator.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
-  user:UserModel
+  user: UserModel;
   role: string;
+  isChecked: boolean;
+  showAlert: boolean;
 
-  constructor(private authService:AuthService) {
-  }
+  constructor(
+    private authService: AuthService,
+    private configService: ConfiguratorService
+  ) {}
 
   ngOnInit(): void {
     this.role = this.authService.getAuthFromLocalStorage().role;
     this.user = this.authService.currentUserValue;
+    this.configService.getConfigurator().subscribe(res => {
+      res.obligated_mode = true;
+      this.isChecked = res.obligated_mode;
+    })
+    
   }
 
   isEmployee() {
     return this.role == "EMPLOYEE";
   }
 
+  isConfigurator() {
+    return this.role == "CONFIGURATOR";
+  }
+  save() {
+    this.configService.updateConfigurations(this.isChecked).subscribe((x) => {
+      this.showAlert = true;
+      setTimeout(() => {
+        this.showAlert = false;
+      }, 5000);
+    });
+  }
 }
